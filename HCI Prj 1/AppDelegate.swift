59698dac63d96a4722c273f33b8ed5f269cdf219
Brendan McAntosh <brendanmcantosh@gmail.com>
2019-02-07 13:13:14 -0600

    Initial Commit

---
+++ HCI Prj 1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HCI Prj 1
+//
+//  Created by Brendan McAntosh on 2/7/19.
+//  Copyright © 2019 Brendan McAntosh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HCI Prj 1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HCI Prj 1
+//
+//  Created by Brendan McAntosh on 2/7/19.
+//  Copyright © 2019 Brendan McAntosh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
